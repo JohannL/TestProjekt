3826ec3da347ee17a889f4ed6052098c88adc0a2
JohannL <github@johann-lau.de>
2021-01-18 15:15:49 +0100

    looping through arrays

--- index.php
+++ index.php
@@ -16,3 +16,26 @@ $also_a_double_because_floats_dont_exist_in_php = '0.0';
 $bool = true;
 $null = NULL;
 
+$indexed_array = [0, 23823, 'poop', false];
+$associative_array = [
+	'a' => 'blah',
+	'b' => 'bleh',
+];
+
+foreach ($indexed_array as $key => $value)
+{
+	//*
+		echo $value;
+	/*/
+		echo $indexed_array[$key];
+	/*/
+}
+
+foreach ($associative_array as $key => $value)
+{
+	//*
+		echo $value;
+	/*/
+		echo $indexed_array[$key];
+	/*/
+}
